189b5384f251aa2dd2d83910adca90466691b5f8
jhyunwoo <jhyunwoo0228@gmail.com>
2022-11-20 00:15:48 +0900

    add google analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,10 +4,14 @@ import { AnalyticsWrapper } from "./components/analytics";
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="bg-orange-50">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-E4L9BS3C6C"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-E4L9BS3C6C');
+        </script>
       <head />
       <body>
         {children}
