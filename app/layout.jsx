b8900d4f320f5240ddc6879e4dca8f1a6b148f34
jhyunwoo <jhyunwoo0228@gmail.com>
2022-11-20 00:24:16 +0900

    add google analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,17 +1,22 @@
 import "./globals.css";
 import { AnalyticsWrapper } from "./components/analytics";
-
+import Script from "next/script";
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="bg-orange-50">
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-E4L9BS3C6C"></script>
-        <script>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-E4L9BS3C6C"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-
+        
           gtag('config', 'G-E4L9BS3C6C');
-        </script>
+        `}
+      </Script>
       <head />
       <body>
         {children}
